832a66fb847324362625428bd8a8f3b77419ed73
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 08:24:10 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in personnummer mha. dialogruta
+        //   - Säg grattis om det är vår födelsedag
+        //   - Meddela att det inte är vår födelsedag
+
+
+    }
+}
